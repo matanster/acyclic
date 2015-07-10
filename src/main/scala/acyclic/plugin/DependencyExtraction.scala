ca3149ef8d8ce54e307db8010dd2931fdd7b7420
matan <dev.matan@gmail.com>
2015-07-10 18:36:53 +0300

    "confirm AST method ids are consistent"

--- src/main/scala/acyclic/plugin/DependencyExtraction.scala
+++ src/main/scala/acyclic/plugin/DependencyExtraction.scala
@@ -7,6 +7,7 @@ object DependencyExtraction{
   def apply(global: Global)(unit: global.CompilationUnit): Seq[(global.Symbol, global.Tree)] = {
     import global._
 
+    @deprecated
     class CollectTypeTraverser[T](pf: PartialFunction[Type, T]) extends TypeTraverser {
       var collected: List[T] = Nil
       def traverse(tpe: Type): Unit = {
@@ -25,6 +26,10 @@ object DependencyExtraction{
       }
     }
 
+    // this only accurately recovers the usage/inclusion hierarchy for naive AST - it does not really grasp
+    // the correct hierarchies, only spits out `println`s per `traverse`s apparent depth-first traversal.
+    // it was very helpful for getting going with rather benign test AST.
+    @deprecated
     class ExtractDependenciesByMemberRefTraverser extends ExtractDependenciesTraverser {
       override def traverse(tree: Tree): Unit = {
         tree match {
@@ -45,8 +50,8 @@ object DependencyExtraction{
             addDependency(select.symbol, tree)
             ////println(select.tpe + " contains symbols: ")
             select.symbol.kindString match {
-              case "method" =>      println("uses: " + select.symbol + " of " + select.symbol.owner + " owned by " + select.symbol.owner.owner)
               case "constructor" => // ignore
+              case "method" =>      println("uses: " + select.symbol + "(" + select.symbol.id + ")" +  " of " + select.symbol.owner + " owned by " + select.symbol.owner.owner)
               case _ =>             println("uses: " + select.symbol + " of type " + select.symbol.tpe.typeSymbol)
             }
             
@@ -63,21 +68,15 @@ object DependencyExtraction{
           case ident: Ident =>
             addDependency(ident.symbol, tree)
             //println(ident.symbol)
-          case typeTree: TypeTree =>
-            //println(typeTree.tpe.typeSymbol)
-            val typeSymbolCollector = new CollectTypeTraverser({
-              case tpe if !tpe.typeSymbol.isPackage => tpe.typeSymbol
-            })
-            typeSymbolCollector.traverse(typeTree.tpe)
-            val deps = typeSymbolCollector.collected.toSet
-            deps.foreach(addDependency(_, tree))
+          case typeTree: TypeTree  => // are we missing something by not handling this?
+          
           case Template(parents, self, body) =>
             val parentTypeSymbols: Set[global.Symbol] = parents.map(parent => parent.tpe.typeSymbol).toSet
             println
             println(tree.tpe.typeSymbol.keyString + " " + tree.tpe.typeSymbol.nameString + " (" + tree.tpe.typeSymbol.id + ") ")
             println("is owned by " + tree.tpe.typeSymbol.owner + " owned by " + tree.tpe.typeSymbol.owner.owner)
             parentTypeSymbols.foreach(s => println("extends: " + s.keyString + " " + s.nameString + " (" + s.id + ") "))
-            tree.tpe.declarations.foreach(s => println("declares own member: " + s.kindString + " " + s.nameString)) // TODO: need to deduplicate these
+            tree.tpe.declarations.foreach(s => println("declares own member: " + s.kindString + " " + s.nameString + "(" + s.id + ")")) // TODO: need to deduplicate these
             traverseTrees(body)
           case tree => super.traverse(tree)
         }
@@ -90,7 +89,7 @@ object DependencyExtraction{
       traverser.dependencies
     }
 
-
+    @deprecated
     class ExtractDependenciesByInheritanceTraverser extends ExtractDependenciesTraverser {
       override def traverse(tree: Tree): Unit = tree match {
         case Template(parents, self, body) =>
@@ -106,15 +105,77 @@ object DependencyExtraction{
       }
     }
 
+    @deprecated
     def byInheritence(): collection.immutable.Set[(Symbol, Tree)] = {
       val traverser = new ExtractDependenciesByInheritanceTraverser
       traverser.traverse(unit.body)
       traverser.dependencies
     }
 
-    val traverseByMembers = byMembers()
-    val traverseByInheritence = byInheritence() 
-    (traverseByMembers | traverseByInheritence).toSeq // get object's dependencies arising from both what's inside 
+    class ExtractAll extends ExtractDependenciesTraverser {
+      override def traverse(tree: Tree): Unit = {
+        tree match {
+          case i @ Import(expr, selectors) =>
+            selectors.foreach {
+              case ImportSelector(nme.WILDCARD, _, null, _) => // a wildcard import
+                // in case of wildcard import we do not rely on any particular name being defined
+                // on `expr`; all symbols that are being used will get caught through selections
+                //println(tree)
+              case ImportSelector(name: Name, _, _, _) => // a specific import
+                def lookupImported(name: Name) = expr.symbol.info.member(name)
+                // importing a name means importing both a term and a type (if they exist)
+                addDependency(lookupImported(name.toTermName), tree)
+                addDependency(lookupImported(name.toTypeName), tree)
+                //println(tree); println(lookupImported(name) + "(" + lookupImported(name).id + ")")
+            }
+          case select: Select =>
+            addDependency(select.symbol, tree)
+            ////println(select.tpe + " contains symbols: ")
+            select.symbol.kindString match {
+              case "constructor" => // ignore
+              case "method" =>      println("uses: " + select.symbol + " (" + select.symbol.id + ")" +  " of " + select.symbol.owner + " owned by " + select.symbol.owner.owner)
+              case _ =>             println("uses: " + select.symbol + " of type " + select.symbol.tpe.typeSymbol)
+            }
+            
+            //select.symbol.tpe.typeSymbol + " or rather of " + select.symbol.owner)
+            
+          /*
+           * Idents are used in number of situations:
+           *  - to refer to local variable
+           *  - to refer to a top-level package (other packages are nested selections)
+           *  - to refer to a term defined in the same package as an enclosing class;
+           *    this looks fishy, see this thread:
+           *    https://groups.google.com/d/topic/scala-internals/Ms9WUAtokLo/discussion
+           */
+          case ident: Ident =>
+            addDependency(ident.symbol, tree)
+            //println(ident.symbol)
+          case typeTree: TypeTree  => // are we missing something by not handling this?
+          
+          case Template(parents, self, body) =>
+            val parentTypeSymbols: Set[global.Symbol] = parents.map(parent => parent.tpe.typeSymbol).toSet
+            println
+            println(tree.tpe.typeSymbol.keyString + " " + tree.tpe.typeSymbol.nameString + " (" + tree.tpe.typeSymbol.id + ") ")
+            println("is owned by " + tree.tpe.typeSymbol.owner + " owned by " + tree.tpe.typeSymbol.owner.owner)
+            parentTypeSymbols.foreach(s => println("extends: " + s.keyString + " " + s.nameString + " (" + s.id + ") "))
+            tree.tpe.declarations.foreach(s => println("declares own member: " + s.kindString + " " + s.nameString + " (" + s.id + ")")) // TODO: need to deduplicate these
+            traverseTrees(body)
+          case tree => super.traverse(tree)
+        }
+      }
+    }
+    
+    def newTraverse(): collection.immutable.Set[(Symbol, Tree)] = {
+      val traverser = new ExtractAll
+      traverser.traverse(unit.body)
+      traverser.dependencies
+    }
+    
+    newTraverse()
+    //val traverseByMembers = byMembers()
+    //val traverseByInheritence = byInheritence() 
+    //(traverseByMembers | traverseByInheritence).toSeq // get object's dependencies arising from both what's inside 
                                                       // it and what's inherited by it (through `extend`).
+    byInheritence().toSeq
   }
 }
